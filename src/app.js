825eb9462543a52bfb6d4f305db7a0d934f03305
alvaradoramon581 <alvaradoramon581@gmail.com>
2026-09-23 15:05:44 -0600

    nueva carpeta

--- src/app.js
+++ src/app.js
@@ -1,35 +1,42 @@
+
 import express from "express";
 import indexRoutes from "./routes/indexRoutes.js";
 import exphbs from "express-handlebars";
-import path from "path";//modulo de node 
-import { fileURLToPath } from "url";
+import path from "path";
 import morgan from "morgan";
 
-// Obtener __dirname en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
+
+// Configuración de vistas
 app.set("views", path.join(__dirname, "views"));
+
+// Configuración de Handlebars
 app.engine(
-    ".hbs",
-    exphbs({
-        layoutsDir: path.join(app.get("views"), "layouts"),
-        defaultLayout: "main",
-        extname: ".hbs",
-    })
+  ".hbs",
+  exphbs({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    defaultLayout: "main",
+    extname: ".hbs"
+  })
 );
+
 app.set("view engine", ".hbs");
 
-//middleware
-app.use(morgan('dev'));
+// Middlewares
+app.use(morgan("dev"));
 
-app.use(express.urlencoded({extended: false}));
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 
-//rutas
+app.use(express.json());
+
+// Rutas
 app.use(indexRoutes);
 
-// /archivos estaticos
-app.use(express.static(path.join(__dirname, "Frontend")))
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, "Frontend")));
 
-export default app;//exporto el objeto app
+export default app;
